7ea0d97e0d51fefd0826e77df7d831965d5c31f6
Artemov Nikita <artnik19@gmail.com>
2017-03-27 11:55:14 +0100

    add headers to response

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const dbUrl = process.env.RG_DBURL;
 
 MongoClient.connect(dbUrl, (err, database) => {
   if (err) return console.log(err)
+  app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
   require('./app/routes')(app, database);
   app.listen(port, () => {
     console.log('We are live on ' + port);
